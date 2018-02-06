dacae114860fd0b1940cd0d80e3605736370033c
Rishabh Dalal <dalalr@uni.edu>
2018-02-05 23:22:23 -0600

    Add files via upload

---
+++ LCS.py
@@ -0,0 +1,19 @@
+##Author: Rishabh Dalal
+##Description: Program to find thee length of longest common substring
+
+def LCSubStr(stringA, stringB):
+    
+    table = [[0 for k in range(len(stringB)+1)] for l in range(len(stringA)+1)]
+    result = 0
+ 
+    for i in range(len(stringA) + 1):
+        for j in range(len(stringB) + 1):
+            if (i == 0 or j == 0):
+                table[i][j] = 0
+            elif (stringA[i-1] == stringB[j-1]):
+                table[i][j] = table[i-1][j-1] + 1
+                result = max(result, table[i][j])
+            else:
+                table[i][j] = 0
+    
+    return result
